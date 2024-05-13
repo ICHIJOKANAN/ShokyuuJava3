0132d90beb19098c7532b05a1dfe508387bf13f1
Ichijo Kanan <23418091@fsgcl.com>
2024-05-13 11:58:33 +0900

    初級Java2.5のコミット

---
+++ 初級JAVA2.5/src/rennsyu21.java
@@ -0,0 +1,21 @@
+
+public class rennsyu21 {
+
+	public static void main(String[] args) {
+		
+		int x = 2;
+		int y = 3;
+		
+		System.out.println(x * y);
+		
+		 MyFrame frame3  = new RectFrame();
+	     frame3.setLocation(50,300);
+	     MyFrame frame4  = new RectFrame();
+         frame4.setLocation(300,300);
+		
+		
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
---
+++ 初級JAVA2.5/src/rennsyu22.java
@@ -0,0 +1,13 @@
+
+public class rennsyu22 extends MyFrame{
+
+	public  void run()
+	{
+		
+		// TODO 自動生成されたメソッド・スタブ
+		  fillRect(50,50,100,100);
+		
+
+	}
+
+}
