8b3214a61f4f4ea9afcfcb566563640f89d72895
Ichijo Kanan <23418091@fsgcl.com>
2024-05-14 12:11:16 +0900

    色の作り方　色の三原色

--- 初級JAVA2.5/src/RectTest.java
+++ 初級JAVA2.5/src/RectTest.java
@@ -3,11 +3,22 @@ public class RectTest extends MyFrame
 {
   public void run()
   {
+	  //黄色
+	  setColor(250,250,0);
 	  fillRect(30, 40,50,100);
+	  //紫色
+	  setColor(250,0,255);
 	  fillRect(130, 40,50,100);
-	  fillRect(30, 200,150,10);
+	  //水色
+	  setColor(0,255,255);
+	  fillRect(230, 40,50,100);
+	  //黒色
+	  setColor(0,0,0);
+	  fillRect(330, 40,50,100);
+	  //灰色
+	  setColor(200,200,200);
+	  fillRect(175, 200,50,100);
+	 
 	  
-	  //鼻？
-	  fillRect(90, 160,30,30);
   }
 }
