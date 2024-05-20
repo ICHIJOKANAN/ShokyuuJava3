0b3edd557853c9cab0b2bfde94c54131b3a53df6
Ichijo Kanan <23418091@fsgcl.com>
2024-05-20 10:20:15 +0900

    練習問題3-1

---
+++ 初級JAVA2.5/src/RectAndOval.java
@@ -0,0 +1,13 @@
+
+public class RectAndOval extends MyFrame
+{
+ public void run() 
+ {
+	 fillRect(40,150,30,15);
+	 fillRect(30,160,50,15);
+     fillOval(35,175,10,10);
+     fillOval(65,175,10,10);
+     
+}
+
+}
