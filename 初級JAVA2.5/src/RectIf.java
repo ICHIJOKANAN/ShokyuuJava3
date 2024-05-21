0d13eccaad7afc2390d19741f3d962b1926626e3
Ichijo Kanan <23418091@fsgcl.com>
2024-05-21 09:46:04 +0900

    練習問題4-3のコミット

--- 初級JAVA2.5/src/RectIf.java
+++ 初級JAVA2.5/src/RectIf.java
@@ -5,15 +5,26 @@ public class RectIf extends MyFrame
   {
 	  int x = 30;
 	  int y = 20;
-	  setColor(0,128,0);
+	  
+	  int a = 0;
+	  int b = 0;
+	  int c = 0;
+	 
 	  int i ; //繰り返しに使う変数iを宣言
 	  for (i = 0; i < 9; i++ ) 
-	  {
 		  
-		fillRect(x,80,10,y);
+	 
+	  {
+	    setColor(a,b,c);
+		fillRect(x,80,10,100);
 		  
 		  x += 20;
 		  y += 20;
+		  
+		  a += 20;
+		  b += 20;
+		  c += 20;
+		 
 		  
 		  
 	  }
