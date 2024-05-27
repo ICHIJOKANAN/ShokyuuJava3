770bd56ea9525ea157807cc86df33605be504cfe
ICHIJO KANAN <zeal23418091@fsgcl.jp>
2024-05-27 14:30:00 +0900

    練習問題5－1のコミット

--- 初級JAVA2.5/src/Animation.java
+++ 初級JAVA2.5/src/Animation.java
@@ -1,43 +1,29 @@
 
 public class Animation extends MyFrame
 {
-  public void run()
-  {
-	  int x =30;
-	  int y = 100;
-	  
+    int y = 200;
+    int x = 30;
+
+    private void MoveRect(int _x, int _y) {
+        //(1)画面を消す
+        clear();
+        //(2)四角形を表示する
+        setColor(0, 128, 0);
+        fillRect(x, y, 10, 100);
+        x += _x;
+        y += _y;
+        sleep(0.1);
+    }
+    public void run() {
+        while (y >= 100) {
+            MoveRect(0, -5);
+        }
+        while (x <= 300) {
+            MoveRect(5, 0);
+        }
+
 		  
-	        while (true) 
-	        {
-	            while (x <= 170)
-	            {
-
-	                //(1)画面を消す
-	                clear();
-
-	                //(2)四角形を表示する
-	                setColor(0, 128, 0);
-	                fillRect(x, y, 10, 100);
-
-	                x += 5;
-	                sleep(0.1);
-	            }
-
-	            while (x >= 30)
-	            {
-
-	                //(1)画面を消す
-	                clear();
-
-	                //(2)四角形を表示する
-	                setColor(0, 128, 0);
-	                fillRect(x, y, 10, 100);
-
-	                x -= 5;
-	                sleep(0.1);
-	            }
-		  
-	        }
-	  
-  }
+	        
+    }
+  
 }
