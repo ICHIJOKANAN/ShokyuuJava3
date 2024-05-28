68c7353043065e131dfc573916d224782c9d6ec8
ICHIJO KANAN <zeal23418091@fsgcl.jp>
2024-05-28 10:43:58 +0900

    6.4 P117のコミット

--- 初級JAVA2.5/src/DisplayFaceObject.java
+++ 初級JAVA2.5/src/DisplayFaceObject.java
@@ -3,12 +3,21 @@ public class DisplayFaceObject extends MyFrame
 {
   public void run()
   {
-	  Face face1=new Face(50,50);
+	  Face face1=new Face(50,50,10,5);
 	 
-	  face1.draw(this);
 	  
-	  Face face2=new Face(200,100);
+	  Face face3=new Face(200,100,-10,-5);
 	 
-	  face2.draw(this);
+	  for (int i= 0; i < 30; i++)
+	  {
+		  clear();
+		  
+		  face1.draw(this);
+		  face3.draw(this);
+		  
+		  face1.move();
+		  face3.move();
+		  sleep(0.5);
+	  }
   }
 }
