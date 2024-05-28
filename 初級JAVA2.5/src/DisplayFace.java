8344171d89bd12e9573e27bea40cd4789e457032
ICHIJO KANAN <zeal23418091@fsgcl.jp>
2024-05-28 11:07:05 +0900

    練習問題6-1のコミット

--- 初級JAVA2.5/src/DisplayFace.java
+++ 初級JAVA2.5/src/DisplayFace.java
@@ -5,9 +5,11 @@ public class DisplayFace extends MyFrame
   {
 	  int x = 30;
 	  int y = 40;
-	  fillOval(x,y,50,100);
-	  fillOval(x+100,y,50,100);
-	  fillRect(x,y+160,150,10);
+	  fillRect(x,y,20,10);
+	  fillRect(x-5,y+8,32,10);
+	  fillOval(x-1,y+17,10,10);
+	  fillOval(x+12,y+17,10,10);
+	  
 	  
 	  int x2 = 180;
 	  int y2 = 120;
