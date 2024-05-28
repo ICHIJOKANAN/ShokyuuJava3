abd962c066eab93101bf334b71818360a5d75f44
ICHIJO KANAN <zeal23418091@fsgcl.jp>
2024-05-28 10:12:02 +0900

    確認問題6.2

--- 初級JAVA2.5/src/DisplayFaceObject.java
+++ 初級JAVA2.5/src/DisplayFaceObject.java
@@ -3,12 +3,16 @@ public class DisplayFaceObject extends MyFrame
 {
   public void run()
   {
-	  Face face1=new Face(50,50);
+	  Face face1=new Face(40,50);
 	 
 	  face1.draw(this);
 	  
-	  Face face2=new Face(200,100);
+	  Face face2=new Face(220,50);
 	 
 	  face2.draw(this);
+	  
+	  Face face3=new Face(125,150);
+		 
+	  face3.draw(this);
   }
 }
