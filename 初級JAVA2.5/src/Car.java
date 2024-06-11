8049b2f070aa73f2f7ebf8739617293543bb3caf
ICHIJO KANAN <zeal23418091@fsgcl.jp>
2024-06-11 10:13:32 +0900

    9.2 P170のコミット

--- 初級JAVA2.5/src/Car.java
+++ 初級JAVA2.5/src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle
+public class Car extends Vehicle implements KeyListener
 { 
 	
 	    public Car(int x, int y, int vx, int vy) {
@@ -12,8 +14,38 @@ public class Car extends Vehicle
 	        frame.fillOval(x + 10, y + 40, 20, 20);
 	        frame.fillOval(x + 50, y + 40, 20, 20);
 	    }
+	    
+	    // キーから文字が入力されたとき
+	    @Override
+	    public void keyTyped(KeyEvent e) {
+	        // TODO 自動生成されたメソッド・スタブ
+
+	    }
+
+	    // キーが押されたとき
+	    @Override
+	    public void keyPressed(KeyEvent e) {
+	        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+	            vx = -5;
+	        }
+	        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+	            vx = 5;
+	        }
+	    }
+
+	    // キーが離されたとき
+	    @Override
+	    public void keyReleased(KeyEvent e) {
+
+	        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+	            vx = 0;
+	        }
+	        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+	            vx = 0;
+	        }
 
     
 
+	    }
 }
 
--- 初級JAVA2.5/src/PlayerAnimation.java
+++ 初級JAVA2.5/src/PlayerAnimation.java
@@ -4,6 +4,8 @@ public class PlayerAnimation extends MyFrame {
 	    public void run() {
 	        Car player =new Car(100,100,0,0); 
 	        
+	        
+	        addKeyListener(player);
 	        while(true) {
 	            clear();
 	            player.draw(this);
